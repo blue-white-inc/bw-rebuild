c6a2d732da38bf8c82f9b1ae3c2b9b1dde26aa68
Alejandro Lazo <alejandrolazo93@gmail.com>
2024-06-06 12:07:27 -0700

    upgarde node-gyp to 10.0.1

--- src/module-type/node-gyp/worker.ts
+++ src/module-type/node-gyp/worker.ts
@@ -1,34 +1,37 @@
-import NodeGypRunner from 'node-gyp';
-import { promisify } from 'util';
+import NodeGypRunner from "node-gyp";
 
-process.on('message', async ({
-  nodeGypArgs,
-  devDir,
-  extraNodeGypArgs,
-}) => {
-  const nodeGyp = NodeGypRunner();
-  nodeGyp.parseArgv(nodeGypArgs);
-  nodeGyp.devDir = devDir;
-  let command = nodeGyp.todo.shift();
-  try {
-    while (command) {
-      if (command.name === 'configure') {
-        command.args = command.args.filter((arg: string) => !extraNodeGypArgs.includes(arg));
-      } else if (command.name === 'build' && process.platform === 'win32') {
-        // This is disgusting but it prevents node-gyp from destroying our MSBuild arguments
-        command.args.map = (fn: (arg: string) => string) => {
-          return Array.prototype.map.call(command.args, (arg: string) => {
-            if (arg.startsWith('/p:')) return arg;
-            return fn(arg);
-          });
+process.on("message", async ({ nodeGypArgs, devDir, extraNodeGypArgs }) => {
+    const nodeGyp = NodeGypRunner();
+    nodeGyp.parseArgv(nodeGypArgs);
+    nodeGyp.devDir = devDir;
+    let command = nodeGyp.todo.shift();
+    try {
+        while (command) {
+            if (command.name === "configure") {
+                command.args = command.args.filter(
+                    (arg: string) => !extraNodeGypArgs.includes(arg)
+                );
+            } else if (
+                command.name === "build" &&
+                process.platform === "win32"
+            ) {
+                // This is disgusting but it prevents node-gyp from destroying our MSBuild arguments
+                command.args.map = (fn: (arg: string) => string) => {
+                    return Array.prototype.map.call(
+                        command.args,
+                        (arg: string) => {
+                            if (arg.startsWith("/p:")) return arg;
+                            return fn(arg);
+                        }
+                    );
+                };
+            }
+            await nodeGyp.commands[command.name](command.args);
+            command = nodeGyp.todo.shift();
         }
-      }
-      await promisify(nodeGyp.commands[command.name])(command.args);
-      command = nodeGyp.todo.shift();
+        process.exit(0);
+    } catch (err) {
+        console.error(err);
+        process.exit(1);
     }
-    process.exit(0);
-  } catch (err) {
-    console.error(err);
-    process.exit(1);
-  }
 });
